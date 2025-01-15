4d2fa236590aeb33b53253d5c0949a3d05f317db
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-01-15 15:14:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ module "spacelift_msteams" {
 
   channel_name = "test"
   space_id     = "root"
-  webhook_url  = "https://prod-98.westus.logic.azure.com:443/workflows/b0f93017fd494640813905f4fe26fe01/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MtZYfhMfSsNALNvyzLtOtgkQySOjD6ZTDsVMdwOoQA8"
+  webhook_url  = "https://prod-90.westus.logic.azure.com:443/workflows/e30b4eaf9a7f4317bcfecf18c80c0500/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=BtUCuBqtVF8_QIjGTaK9pSFSQeqIjwIkeKYC55t3BX4"
 }
